ce9582de504f6b42d3426664df76698112e937e1
Rod512 <rudro149187@bsdi-bd.org>
2024-03-21 02:07:42 +0530

    admin panel customize

--- realstate/settings.py
+++ realstate/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
 
@@ -128,3 +129,7 @@ STATICFILES_DIRS = [STATIC_DIR]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media file configure
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- realtors/models.py
+++ realtors/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Realtor(models.Model):
     name = models.CharField(max_length= 200)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    description = models.CharField(max_length= 200)
+    description = models.TextField(max_length= 500)
     phone = models.CharField(max_length= 50)
     email = models.CharField(max_length= 50)
     is_mvp = models.BooleanField(default=False)
